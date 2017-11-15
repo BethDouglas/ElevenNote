b923e4d49be807de43d997ba9025717c1770c954
Beth Douglas <bethDdouglas@gmail.com>
2017-11-15 14:32:29 -0500

    Connected controller to service

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElevenNote.Data;
+using ElevenNote.Models;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        public IEnumerable<NoteListItemModel> GetNotes()
+        {
+            using (var ctx = new ElevenNoteDbContext())
+            {
+                return
+                    ctx
+                        .Notes
+                        .Select(
+                            e => 
+                                new NoteListItemModel
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc,
+                                    ModifiedUtc = e.ModifiedUtc
+                                })
+                        .ToArray();
+
+            }
+        }
+    }
+}
