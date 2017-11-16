8231c65aaf2618b5b6f1a7b3bb72173d9a4ec4b8
Beth Douglas <bethDdouglas@gmail.com>
2017-11-16 16:29:21 -0500

    Added Star feature

--- ElevenNote.Models/NoteListItemModel.cs
+++ ElevenNote.Models/NoteListItemModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,10 @@ namespace ElevenNote.Models
         public int NoteId { get; set; }
 
         public string Title { get; set; }
+
+        [UIHint("Starred")]
+        [DisplayName("Important")]
+        public bool IsStarred { get; set; }
 
         public DateTime CreatedUtc { get; set; }
 
